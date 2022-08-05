dfcf838d15dd3db41b47ad264bd4fe7d0114d132
LUIZ SOUZA <luiz.souza@mobicare.com.br>
2022-08-04 22:09:13 -0300

    refact: improve test with a verifyGreeting

--- greeting/greeting_test.go
+++ greeting/greeting_test.go
@@ -3,41 +3,40 @@ package main
 import "testing"
 
 func TestGreeting(t *testing.T) {
+	verifyGreeting := func(t *testing.T, got, want string) {
+		t.Helper()
+
+		if got != want {
+			t.Errorf("Greeting\nexpect: %v\ngot: %v", want, got)
+		}
+	}
 
 	t.Run("This should returns 'Olá, Luiz' pass in args", func(t *testing.T) {
 		got := Greeting("Luiz", "")
 		want := "Olá, Luiz"
 
-		if got != want {
-			t.Errorf("Greeting\nexpect: %v\ngot: %v", want, got)
-		}
+		verifyGreeting(t, got, want)
 	})
 
 	t.Run("This test should returns 'Olá, mundo' if args is empty", func(t *testing.T) {
 		got := Greeting("", "")
 		want := "Olá, mundo"
 
-		if got != want {
-			t.Errorf("Greeting\nexpect: %v\ngot: %v", want, got)
-		}
+		verifyGreeting(t, got, want)
 	})
 
 	t.Run("This test should returns 'Hello, Luiz'", func(t *testing.T) {
 		got := Greeting("Luiz", "english")
 		want := "Hello, Luiz"
 
-		if got != want {
-			t.Errorf("Greeting\nexpect: %v\ngot: %v", got, want)
-		}
+		verifyGreeting(t, got, want)
 	})
 
 	t.Run("This test should returns 'Bonjour, Luiz'", func(t *testing.T) {
 		got := Greeting("Luiz", "french")
 		want := "Bonjour, Luiz"
 
-		if got != want {
-			t.Errorf("Greeting\nexpect: %v\ngot: %v", got, want)
-		}
+		verifyGreeting(t, got, want)
 	})
 
 }
