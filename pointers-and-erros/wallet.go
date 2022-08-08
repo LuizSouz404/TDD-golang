0d5a48f99d7832d7bf48c78a1b8443c4efa3123a
LUIZ SOUZA <luiz.souza@mobicare.com.br>
2022-08-07 22:06:45 -0300

    refact: refact test, remove duplicate code

--- pointers-and-erros/wallet.go
+++ pointers-and-erros/wallet.go
@@ -1,11 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {}
 
-type Bitcoin float64
+type Bitcoin int
 
 type Wallet struct {
 	Statement Bitcoin
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
 func (w *Wallet) Deposit(value Bitcoin) {
@@ -14,4 +24,8 @@ func (w *Wallet) Deposit(value Bitcoin) {
 
 func (w *Wallet) Extract() Bitcoin {
 	return w.Statement
+}
+
+func (w *Wallet) WithDraw(value Bitcoin) {
+	w.Statement -= value
 }
--- pointers-and-erros/wallet_test.go
+++ pointers-and-erros/wallet_test.go
@@ -3,14 +3,32 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	confirmStatement := func(t *testing.T, wallet Wallet, valueWant Bitcoin) {
+		got := wallet.Extract()
 
-	wallet.Deposit(10)
-
-	got := wallet.Extract()
-	want := Bitcoin(10.0)
-
-	if got != want {
-		t.Errorf("Wallet\ngot: %2.f\nexpect: %2.f", got, want)
+		if got != valueWant {
+			t.Errorf("Wallet\ngot: %d\nexpect: %d", got, valueWant)
+		}
 	}
+
+	t.Run("This test should deposit 10 BTC in test wallet", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(10)
+
+		want := Bitcoin(10)
+
+		confirmStatement(t, wallet, want)
+	})
+
+	t.Run("This test should withdraw 10 BTC in test wallet", func(t *testing.T) {
+		wallet := Wallet{Statement: Bitcoin(20)}
+
+		wallet.WithDraw(Bitcoin(10))
+
+		want := Bitcoin(10)
+
+		confirmStatement(t, wallet, want)
+	})
+
 }
