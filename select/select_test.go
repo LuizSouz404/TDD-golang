3a02373ccb6cbb5814eb94c4fe2363cc5442546f
LuizSouz404 <luiz.souz404@gmail.com>
2022-08-12 18:00:05 -0300

    tests: create fale server to test funcionallity

--- select/select_test.go
+++ select/select_test.go
@@ -1,10 +1,24 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestSelect(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://quii.com.uk"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Runner(slowURL, fastURL)
@@ -12,4 +26,6 @@ func TestSelect(t *testing.T) {
 	if got != want {
 		t.Errorf("Runner\ngot: %s\nexpect: %s", got, want)
 	}
+	slowServer.Close()
+	fastServer.Close()
 }
