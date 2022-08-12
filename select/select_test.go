9ee2d7c412071d52b1ddf172339d1d93c4558f60
LuizSouz404 <luiz.souz404@gmail.com>
2022-08-12 18:10:33 -0300

    refact: refact tests with a new create server func

--- select/select_test.go
+++ select/select_test.go
@@ -8,17 +8,14 @@ import (
 )
 
 func TestSelect(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(20 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
-
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
+	slowServer := createServerWithDelay(20 * time.Millisecond)
+	fastServer := createServerWithDelay(0 * time.Millisecond)
 
 	slowURL := slowServer.URL
 	fastURL := fastServer.URL
+
+	defer slowServer.Close()
+	defer fastServer.Close()
 
 	want := fastURL
 	got := Runner(slowURL, fastURL)
@@ -26,6 +23,11 @@ func TestSelect(t *testing.T) {
 	if got != want {
 		t.Errorf("Runner\ngot: %s\nexpect: %s", got, want)
 	}
-	slowServer.Close()
-	fastServer.Close()
+}
+
+func createServerWithDelay(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
