cbbc1c4c102b510a07e44bf4ec846250b122ccff
LuizSouz404 <luiz.souz404@gmail.com>
2022-08-14 17:49:30 -0300

    tests: create test to check if returns data from store

--- context/context.go
+++ context/context.go
@@ -9,10 +9,12 @@ func main() {}
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		store.Cancel()
 		fmt.Fprint(w, store.Fetch())
 	}
 }
 
 type Store interface {
 	Fetch() string
+	Cancel()
 }
--- context/context_test.go
+++ context/context_test.go
@@ -1,29 +1,68 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 type StubStore struct {
 	response string
 }
 
-func (s *StubStore) Fetch() string {
+type SpyStore struct {
+	response  string
+	cancelled bool
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
 	return s.response
 }
 
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
+
 func TestHandler(t *testing.T) {
-	data := "hello, world"
-	svr := Server(&StubStore{data})
+	t.Run("Return data", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data}
+		svr := Server(store)
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
 
-	svr.ServeHTTP(response, request)
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
 
-	if response.Body.String() != data {
-		t.Errorf("Context\ngot: %s\nexpect: %s", response.Body.String(), data)
-	}
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if !store.cancelled {
+			t.Errorf("Context\nStore was not warn to cancel")
+		}
+	})
+
+	t.Run("Return data from store", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if response.Body.String() != data {
+			t.Errorf("Context\ngot: '%s'\nexpect: '%s'", response.Body.String(), data)
+		}
+
+		if !store.cancelled {
+			t.Errorf("Context\nShould not cancel from store")
+		}
+	})
 }
